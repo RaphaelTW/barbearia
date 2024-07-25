256c46b69e47dc07b4d762e8ca5c2685bcb58dc3
Raphael Silva | Via Expressa Soluções Logísticas <raphael.silva@viaexpressa.com>
2024-07-25 14:53:06 -0300

    configurando o backend

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { DbModule } from './db/db.module';
+import { ServicoModule } from './servico/servico.module';
+import { AgendamentoModule } from './agendamento/agendamento.module';
+// import { AppService } from './app.service';
+// import { AppController } from './app.controller';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [DbModule, ServicoModule, AgendamentoModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
